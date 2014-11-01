2c6f2d6b3e297849b6d0e0a5bb984ab546d88399
Jianhe Luo <jianhel@andrew.cmu.edu>
2014-11-01 09:38:51 +0800

    put the function about delete node in the tree

--- BinaryTree2/src/BinaryTree.java
+++ BinaryTree2/src/BinaryTree.java
@@ -13,6 +13,7 @@ public class BinaryTree {
 	//constructor function
 	public BinaryTree() {
 		super();
+		node_number = 0;
 	}
 	
 	
@@ -40,9 +41,15 @@ public class BinaryTree {
 	public BinaryTree insert(int value){
 		if(null !=root)
 			insert(root,value);
-		else
+		else{
 			root = new TreeNode(value);
+		}
+		
+		node_number++;
+		
 		return this;
+		
+		
 	}
 	
 	//very important
@@ -109,8 +116,7 @@ public class BinaryTree {
 				return true;
 			}
 		}
-		}
-	
+	}
 	
 
 	//root left right
@@ -271,18 +277,190 @@ public class BinaryTree {
 			while(!queue.isEmpty()){
 				
 				if(queue.peek().getLeftchild()!=null){
+					
+					//put the node into the queue
 					queue.offer(queue.peek().getLeftchild());
 				}
 				if(queue.peek().getRightchild()!=null){
+					
+					//put the node into the queue
 					queue.offer(queue.peek().getRightchild());
 				}
 				
+				//put the node out of the node
 				sb.append(queue.poll().getValue()).append(" ");
 			}
 			System.out.println(sb.toString());
 		}
 	}
 	
+	//delete the node 
+	public boolean delete(int value){
+		if(root==null){
+			return false;
+		}
+		else{
+			return delete(root,value);
+		}	
+	}
+	
+	//define the function of deleting the node
+	public boolean delete(TreeNode node, int value){
+		if(value<node.getValue()){
+			if(node.getLeftchild()!=null){
+				return delete(node.getLeftchild(),value);
+			}
+			else{
+				return false;
+			}
+		}
+		else if(value>node.getValue()){
+			if(node.getRightchild()!=null){
+				return delete(node.getRightchild(),value);
+			}
+			else{
+				return false;
+			}
+		}
+		//找到正确的点，将点删除后需要进行点的replace
+		else{
+			//左孩子为空，右孩子也为空。
+			if(node.getLeftchild()==null&&node.getRightchild()==null){
+				//直接使节点为null即可
+				node = null;
+				return true;
+			}
+			
+			//左孩子不为空，右孩子为空
+			else if(node.getLeftchild()!=null&&node.getRightchild()==null){
+				//调用函数
+				delete_nodewithleftchild(node);
+				return true;
+			}
+			
+			//右孩子不为空，左孩子为空
+			else if(node.getRightchild()!=null&&node.getLeftchild()==null){
+				//调用函数
+				delete_nodewithrightchild(node);
+				return true;
+			}
+			
+			//左孩子不为空，有孩子不为空
+			else if(node.getLeftchild()!=null&&node.getRightchild()!=null){
+				//调用函数
+				delete_nodewithtwochild(node);
+				return true;
+			}
+			return true;
+		}
+	}	
+	
+	//删除的节点有左子树
+	private TreeNode delete_nodewithleftchild(TreeNode node){
+		TreeNode temp = new TreeNode();
+		//调用函数找到左子树的最大节点
+		temp = findLargestNode(node.getLeftchild());
+		node.setValue(temp.getValue());
+		temp = null;
+		
+		return node;
+	}
+	
+	//删除的节点有右子树
+	private TreeNode delete_nodewithrightchild(TreeNode node){
+		TreeNode temp = new TreeNode();
+		//调用函数找到右子树的最小节点
+		temp = findSmallestNode(node.getRightchild());
+		node.setValue(temp.getValue());
+		temp = null;
+		
+		return node;
+	}
+
+	//删除的节点有左右子树
+	private TreeNode delete_nodewithtwochild(TreeNode node){
+	
+		TreeNode temp1 = new TreeNode();
+		TreeNode temp2 = new TreeNode();
+		
+		//左子树的最大值
+		temp1 = findLargestNode(node.getLeftchild());
+		//右子树的最小值
+		temp2 = findSmallestNode(node.getRightchild());
+		
+		//既有左子树，又有右子树，则有两种选择，可以选择用左子树的最大节点replace或者选择右子树的最小节点replace
+		node.setValue(temp2.getValue());
+		temp2 = null;
+		
+		return node;
+	}
+
+	
+	//找到树的最左边节点，即最小节点
+	public TreeNode findLeftestNoe(TreeNode node){
+		
+		TreeNode temp = new TreeNode(node);
+		
+		while(temp.getLeftchild()!=null){
+			temp.setValue(temp.getLeftchild().getValue());
+			temp.setLeftchild(temp.getLeftchild().getLeftchild());
+//			temp.setRightchild(temp.getLeftchild().getRightchild());
+		}
+		return temp;
+	}
+	
+	//找到树的最右边节点，即最大节点
+	public TreeNode findRightestNode(TreeNode node){
+		TreeNode temp = new TreeNode(node);
+		
+		while(temp.getRightchild()!=null){
+			temp.setValue(temp.getRightchild().getValue());
+			temp.setRightchild(temp.getRightchild().getRightchild());
+//			temp.setLeftchild(temp.getRightchild().getLeftchild());
+		}
+		return temp;
+	}
+	
+	
+	//find out the smallest node, and put the variable node as the root of a tree
+	public TreeNode findSmallestNode(TreeNode node){
+		
+		if(node.getLeftchild()!=null&&node.getRightchild()!=null){
+			return findSmallestNode(node.getLeftchild());
+		}
+		else if(node.getLeftchild()!=null&&node.getRightchild()==null){
+			return findSmallestNode(node.getLeftchild());
+		}
+		else if(node.getLeftchild()==null&&node.getRightchild()!=null){
+			return node;
+		}
+		else{
+			return node;
+		}
+	}
+
+	
+	//find out the largest node, and put the variable node as the root of a tree
+	public TreeNode findLargestNode(TreeNode node){
+		if (node.getLeftchild()!=null&&node.getRightchild()!=null){
+			return findLargestNode(node.getRightchild());
+		}
+		else if(node.getLeftchild()==null&&node.getRightchild()!=null){
+			return findLargestNode(node.getRightchild());
+		}
+		else if(node.getLeftchild()!=null&&node.getRightchild()==null){
+			return node;
+		}
+		else{
+			return node;
+		}
+	}
+	
+	
+	
+	
+	
+
 	
 	
 	
--- BinaryTree2/src/Testing.java
+++ BinaryTree2/src/Testing.java
@@ -3,15 +3,20 @@ public class Testing {
 
 	public static void main(String [] args){
 		
-		TreeNode node1 = new TreeNode(5);
-		TreeNode node2 = new TreeNode(3);
-		TreeNode node3 = new TreeNode(1);
-		TreeNode node4 = new TreeNode(4);
-		TreeNode node5 = new TreeNode(7);
-		TreeNode node6 = new TreeNode(6);
-		TreeNode node7 = new TreeNode(8);
+//		TreeNode node1 = new TreeNode(5);
+//		TreeNode node2 = new TreeNode(3);
+//		TreeNode node3 = new TreeNode(1);
+//		TreeNode node4 = new TreeNode(4);
+//		TreeNode node5 = new TreeNode(7);
+//		TreeNode node6 = new TreeNode(6);
+//		TreeNode node7 = new TreeNode(8);
+//		
+//		System.out.println(node1);
 		
-		System.out.println(node1);
+		
+		System.out.println("**********************");
+		System.out.println("Start a new tree.");
+		System.out.println("**********************");
 		
 		BinaryTree bt = new BinaryTree();
 		
@@ -39,6 +44,23 @@ public class Testing {
 		System.out.println("insert the node 8.");
 		bt.insert(8);
 		
+		
+		TreeNode node_largest = new TreeNode();
+		node_largest = bt.findLargestNode(bt.getRoot());
+		System.out.println("the largest value of the tree is:"+node_largest.getValue());
+		
+		TreeNode node_smallest = new TreeNode();
+		node_smallest = bt.findSmallestNode(bt.getRoot());
+		System.out.println("the smallest value of the tree is:"+node_smallest.getValue());
+		
+		TreeNode node_leftest = new TreeNode();
+		node_leftest = bt.findLeftestNoe(bt.getRoot());
+		System.out.println("the leftest node of the tree is:"+node_leftest.getValue());
+		
+		TreeNode node_rightest = new TreeNode();
+		node_rightest = bt.findRightestNode(bt.getRoot());
+		System.out.println("the rightest node of the tree is:"+node_rightest.getValue());
+		
 		System.out.println("the root of the tree is:"+bt.getRoot().getValue());
 		
 		System.out.println("the leftchild of the root is:"+bt.getRoot().getLeftchild().getValue());
@@ -54,7 +76,6 @@ public class Testing {
 		System.out.println("the rightchild of the rightchild of the root is:"+bt.getRoot().getRightchild().getRightchild().getValue());
 		
 		System.out.println("the val_test is:"+bt.find(10));
-		
 		
 		System.out.println(bt);
 		
@@ -80,9 +101,132 @@ public class Testing {
 		
 		System.out.println("***************************");
 		
-		System.out.println("*******~~********************");
+		System.out.println("***************************");
 		bt.display_BFS();
 		System.out.println("***************************");
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("**********************");
+		System.out.println("Start a new tree.");
+		System.out.println("**********************");
+		
+		
+		System.out.println();
+		
+		BinaryTree bt2 = new BinaryTree();
+		
+		System.out.println("insert the node 9.");
+		bt2.insert(9);
+		System.out.println("the root is:"+bt2.getRoot().getValue());
+		
+		System.out.println("insert the node 8.");
+		bt2.insert(8);
+		
+		System.out.println("insert the node 7.");
+		bt2.insert(7);
+		
+		System.out.println("insert the node 3.");
+		bt2.insert(3);
+		
+		System.out.println("insert the node 4.");
+		bt2.insert(4);
+		
+		System.out.println("insert the node 5.");
+		bt2.insert(5);
+		
+		System.out.println("insert the node 6.");
+		bt2.insert(6);
+		
+		System.out.println("the root of the tree is:"+bt2.getRoot().getValue());
+		
+		System.out.println("the leftchild of the root is:"+bt2.getRoot().getLeftchild().getValue());
+		
+		System.out.println("the leftchild of the leftchild of the root is:"+bt2.getRoot().getLeftchild().getLeftchild().getValue());
+		
+		System.out.println("the val_test is:"+bt.find(6));
+		
+		TreeNode node_smallest2 = new TreeNode();
+		node_smallest2 = bt2.findSmallestNode(bt2.getRoot());
+		System.out.println("the smallest value of the tree2 is:"+node_smallest2.getValue());
+		
+		TreeNode node_largest2 = new TreeNode();
+		node_largest2 = bt2.findLargestNode(bt2.getRoot());
+		System.out.println("the largest value of the tree2 is:"+node_largest2.getValue());
+		
+		TreeNode node_leftest2 = new TreeNode();
+		node_leftest2 = bt2.findLeftestNoe(bt2.getRoot());
+		System.out.println("the leftest node of the tree is:"+node_leftest2.getValue());
+		
+		TreeNode node_rightest2 = new TreeNode();
+		node_rightest2 = bt2.findRightestNode(bt2.getRoot());
+		System.out.println("the rightest node of the tree is:"+node_rightest2.getValue());
+		
+		
+		
+		
+		
+		
+		
+		
+
+		System.out.println("**********************");
+		System.out.println("Start a new tree.");
+		System.out.println("**********************");
+		
+		BinaryTree bt3 = new BinaryTree();
+		
+		System.out.println("insert the node 3.");
+		bt3.insert(3);
+		System.out.println("the root is:"+bt3.getRoot().getValue());
+		
+		System.out.println("insert the node 4.");
+		bt3.insert(4);
+		
+		System.out.println("insert the node 9.");
+		bt3.insert(9);
+		
+		System.out.println("insert the node 7.");
+		bt3.insert(7);
+		
+		System.out.println("insert the node 6.");
+		bt3.insert(6);
+		
+		System.out.println("insert the node 5.");
+		bt3.insert(5);
+		
+		System.out.println("insert the node 2.");
+		bt3.insert(2);
+		
+		TreeNode node_smallest3 = new TreeNode();
+		node_smallest3 = bt3.findSmallestNode(bt3.getRoot());
+		System.out.println("the smallest value of the tree2 is:"+node_smallest3.getValue());
+		
+		TreeNode node_largest3 = new TreeNode();
+		node_largest3 = bt3.findLargestNode(bt3.getRoot());
+		System.out.println("the largest value of the tree2 is:"+node_largest3.getValue());
+		
+		TreeNode node_leftest3 = new TreeNode();
+		node_leftest3 = bt3.findLeftestNoe(bt3.getRoot());
+		System.out.println("the leftest node of the tree is:"+node_leftest3.getValue());
+		
+		TreeNode node_rightest3 = new TreeNode();
+		node_rightest3 = bt3.findRightestNode(bt2.getRoot());
+		System.out.println("the rightest node of the tree is:"+node_rightest3.getValue());
+		
+		
+		
+		
+		boolean m =  bt3.delete(4);
+		
+		System.out.println("true:"+m);
+		
+		System.out.println("the root of the tree is:"+bt3.getRoot().getValue());
 		
 	}
 }
